04b4e6de092e322b069019a43398ed2edcf0b7f2
samedan <same.dan@gmail.com>
2020-01-27 15:00:49 +0100

    End Sect 6. Complete Menu

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -2,14 +2,35 @@ import React, { Fragment } from 'react';
 import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
 import NavBar from '../../features/nav/NavBar/NavBar';
 import { Container } from 'semantic-ui-react';
+import { Route } from 'react-router-dom';
+import HomePage from '../../features/home/HomePage';
+import EventDetailedPage from '../../features/event/EventDetailed/EventDetailedPage';
+import PeopleDashboard from '../../features/user/PeopleDashboard/PeopleDashboard';
+import UserDetailedPage from '../../features/user/UserDetailed/UserDetailedPage';
+import SettingsDashboard from '../../features/user/Settings/SettingsDashboard';
+import EventForm from '../../features/event/EventForm/EventForm';
 
 function App() {
   return (
     <Fragment>
-      <NavBar />
-      <Container className="main">
-        <EventDashboard />
-      </Container>
+      <Route exact path="/" component={HomePage} />
+      <Route
+        path="/(.+)"
+        render={() => (
+          <Fragment>
+            <NavBar />
+
+            <Container className="main">
+              <Route path="/events" component={EventDashboard} />
+              <Route path="/events/:id" component={EventDetailedPage} />
+              <Route path="/people" component={PeopleDashboard} />
+              <Route path="/profile/:id" component={UserDetailedPage} />
+              <Route path="/settings" component={SettingsDashboard} />
+              <Route path="/createEvent" component={EventForm} />
+            </Container>
+          </Fragment>
+        )}
+      />
     </Fragment>
   );
 }
---
+++ src/features/nav/Menus/SignedOutMenu.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Menu, Button } from 'semantic-ui-react';
+
+const SignedOutMenu = ({ signIn }) => {
+  return (
+    <div>
+      <Menu.Item position="right">
+        <Button basic inverted content="Login" onClick={signIn} />
+        <Button
+          basic
+          inverted
+          content="Register"
+          style={{ marginLeft: '0.5em' }}
+        />
+      </Menu.Item>
+    </div>
+  );
+};
+
+export default SignedOutMenu;
